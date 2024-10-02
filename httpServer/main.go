143078144592275b10bfe77da7cda9c986c08fec
Pratyush <oggycandle786@gmail.com>
2024-10-02 12:24:37 +0530

    Adding rel stuff

--- httpServer/main.go
+++ httpServer/main.go
@@ -8,49 +8,49 @@ import (
 
 const PORT string = ":8080"
 
-func greetingMethod(w http.ResponseWriter, r *http.Request) {
+func greetingYou(w http.ResponseWriter, r *http.Request) {
 
 	if r.URL.Path != "/home" {
-		http.Error(w, "Sorry You might land up in wrong page", http.StatusNotFound)
+		http.Error(w, "It is not the expected path that we want and you need to improve ", http.StatusBadRequest)
 		return
 	}
 
 	if r.Method != "GET" {
-		http.Error(w, "You are hitting wrong request", http.StatusBadRequest)
+		http.Error(w, "It is not the expected method that we expected and you need to improve it ", http.StatusNotFound)
 		return
 	}
-	fmt.Fprintf(w, "Welcome I am Pratyush")
-	fmt.Println(w, "I am Pratyush and Welcome")
+
+	fmt.Fprintln(w, "Welcome back , I am pratyush and learning to code in golang")
+	fmt.Fprintln(w, "Please help me in moving my journey and want to be great as an engineer")
+
 }
 
-func postValue(w http.ResponseWriter, r *http.Request) {
+func fillTheForm(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/form" {
+		fmt.Fprintln(w, "It is not the correct path that you have chosen and we have better expectation than you")
+		return
+	}
 
 	if err := r.ParseForm(); err != nil {
-
-		log.Printf("Error exist and kuch nahi kiya jaa sakt hai %v", err)
+		panic(err)
 	}
 
 	name := r.FormValue("name")
 	address := r.FormValue("address")
-
-	fmt.Fprintf(w, "Your name gentleman is %s", name)
-	fmt.Fprintf(w, "Your address gentleman is %s", address)
-
-	//fmt.Println(name)
-	//fmt.Println(address)
+	fmt.Fprintf(w, "Hey %s , How are You/n", name)
+	fmt.Fprintf(w, "You address is %s as you mentioned", address)
 }
 
 func main() {
-
-	log.Println("Http Server is running peacefully and working fine")
+	log.Println("I am trying to build a new project on golang")
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
-	http.HandleFunc("/home", greetingMethod)
-	http.HandleFunc("/form", postValue)
+	http.HandleFunc("/home", greetingYou)
+	http.HandleFunc("/form", fillTheForm)
 
 	err := http.ListenAndServe(PORT, nil)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
