cd2ba103a45217ac6b7e529f7827eefec6ca6597
Pratyush <oggycandle786@gmail.com>
2024-10-09 22:28:48 +0530

    Adding something

--- curd-api/main.go
+++ curd-api/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+const PORT string = ":8080"
+
 func main() {
 
+	router := gin.Default()
+
+	router.GET("/ping", func(g *gin.Context) {
+		g.JSON(http.StatusAccepted, gin.H{
+			"message": "pong",
+		})
+	})
+
+	log.Fatal(router.Run(PORT))
 }
