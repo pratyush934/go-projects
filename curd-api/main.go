1db99f0a4b45c7236012831e70441e39a377ac7d
Pratyush <oggycandle786@gmail.com>
2024-10-11 22:01:45 +0530

    Adding something relevant

--- curd-api/main.go
+++ curd-api/main.go
@@ -1,22 +1,25 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"log"
-	"net/http"
-)
+import "fmt"
 
 const PORT string = ":8080"
 
+func divide(num, den float64) float64 {
+	if den == 0 {
+		/*
+			panic function is inbuilt function which is defined under the built-in package of golang
+			this function terminates the flow of control and starts panicking
+			To handle the error as specified the
+			panic function also give information about the type of error or the location of the
+		*/
+		panic("Cannot be divided by 0")
+		//fmt.Println("Can not")
+	}
+	return num / den
+}
 func main() {
-
-	router := gin.Default()
-
-	router.GET("/ping", func(g *gin.Context) {
-		g.JSON(http.StatusAccepted, gin.H{
-			"message": "pong",
-		})
-	})
-
-	log.Fatal(router.Run(PORT))
+	defer fmt.Println("No No")
+	fmt.Println("So Sad")
+	defer fmt.Println("Good Going")
+	fmt.Println("Not Good Going")
 }
