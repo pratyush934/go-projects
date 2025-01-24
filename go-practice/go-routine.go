67c0823cd6b912b4f2c3aa52d942e949ab9bd692
Pratyush <oggycandle786@gmail.com>
2025-01-24 15:38:02 +0530

    Adding something relevant

--- go-practice/go-routine.go
+++ go-practice/go-routine.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net"
 	"time"
 )
 
@@ -32,6 +35,38 @@ func experiment1() {
 	fmt.Println("THe fibonacci here is ", fiboNacci)
 }
 
+func networkConnection() {
+
+	listen, err := net.Listen("tcp", "localhost:8080")
+
+	if err != nil {
+		log.Fatal("There is error and we need to fix this", err)
+	}
+
+	for {
+		conn, err := listen.Accept()
+
+		if err != nil {
+			log.Fatal("There was an error and program seems to be aborted", err)
+		}
+
+		handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	for {
+		_, err := io.WriteString(conn, time.Now().Format("02:01:2006"))
+
+		if err != nil {
+			log.Fatal("Not so good but , Not so good", err)
+			return
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
 func main() {
-	experiment1()
+	networkConnection()
 }
